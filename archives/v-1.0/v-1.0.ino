0f3cbb6b4f024271036767031057302c86506c5d
Nch-MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-03-21 12:02:18 +0900

    Update v-1.0.ino

--- archives/v-1.0/v-1.0.ino
+++ archives/v-1.0/v-1.0.ino
@@ -1,1 +1,31 @@
+#include <DigiKeyboard.h>
 
+byte Pin0_old_data = 1;
+byte Pin2_old_data = 1;
+byte Pin0_new_data = 1;
+byte Pin2_new_data = 1;
+
+void setup() {
+  pinMode(0, INPUT);
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  if (Pin0_old_data == 1 && Pin0_new_data == 0){
+    DigiKeyboard.println(F("Text_1"));
+  }
+
+  if (Pin2_old_data == 1 && Pin2_new_data == 0){
+    DigiKeyboard.println(F("Text_2"));
+  }
+
+  Pin0_old_data = Pin0_new_data;
+  Pin2_old_data = Pin2_new_data;
+
+  Pin0_new_data = digitalRead(0);   
+  Pin2_new_data = digitalRead(2); 
+
+  DigiKeyboard.sendKeyStroke(0);
+
+  delay(50);
+}
